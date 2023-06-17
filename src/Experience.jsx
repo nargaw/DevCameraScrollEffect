32a9524641237a65137474a445413c26a6071cd3
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-06-17 05:43:14 -0400

    transparency issues

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -16,11 +16,46 @@ export default function Experience()
 
     return <>
         {/* Orbit Controls */}
-        <OrbitControls makeDefault enableZoom={true} enablePan={false}/>
-        <fog attach="fog" args={[0xffffff, 6, 7]} />
-        <Stage adjustCamera={1.5}  intensity={0.5} shadows="contact" > 
+        <OrbitControls 
+            makeDefault
+            enableDamping={true} 
+            enableZoom={true} 
+            enablePan={false}
+            maxAzimuthAngle={Math.PI / 2}
+            maxPolarAngle={Math.PI}
+            minAzimuthAngle={-Math.PI / 2}
+            minPolarAngle={-Math.PI}
+        />
+        <group> 
+            <mesh>
+                <cylinderGeometry args={[0.5, 0.5, 0.4]}/>
+                <meshStandardMaterial />
+            </mesh>
+            <mesh position={[0, 1., 0.]}>
+                <boxGeometry args={[0.5, 1.75, 0.5]}/>
+                <meshStandardMaterial transparent={true} opacity={0.2}/>
+            </mesh>
+            <mesh position={[-0.5, 1.45, 0]}>
+                <boxGeometry args={[1, 0.1, 0.1]} />
+                <meshStandardMaterial />
+            </mesh>
+            <mesh position={[0.5, 1.45, 0]}>
+                <boxGeometry args={[1, 0.1, 0.1]} />
+                <meshStandardMaterial />
+            </mesh>
+            <mesh position={[0, 0.85, 0]}>
+                <boxGeometry args={[0.25, 0.25, 0.25]} />
+                <meshStandardMaterial transparent={true} opacity={0.25} color={0x0000ff} side={THREE.DoubleSide}/>
+            </mesh>
+            <mesh position={[0, 0.85, 0]}>
+                <boxGeometry args={[0.125, 0.125, 0.125]} />
+                <meshPhongMaterial transparent={true} opacity={0.75} color={0x00ff00} side={THREE.DoubleSide}/>
+            </mesh>
+        </group>
+        {/* <fog attach="fog" args={[0xffffff, 6, 7]} /> */}
+        <Stage adjustCamera={1.5}  intensity={0.05} shadows="contact" > 
             {/* control presentation - user rotation of model */}
-            <PresentationControls 
+            {/* <PresentationControls 
                 global 
                 rotation={ [ 0., 0.0, 0 ] }
                 polar={ [ 0, 0 ] }
@@ -28,16 +63,16 @@ export default function Experience()
                 // azimuth={ [ 0,  0 ] }
                 config={ { mass: 2, tension: 50 } }
                 snap={ { mass: 2, tension: 50 } }
-            >
+            > */}
                 {/* Model */}
-                <group> 
+                {/* <group> 
                     <mesh>
                         <cylinderGeometry args={[0.5, 0.5, 0.4]}/>
                         <meshStandardMaterial />
                     </mesh>
                     <mesh position={[0, 1., 0.]}>
                         <boxGeometry args={[0.5, 1.75, 0.5]}/>
-                        <meshStandardMaterial transparent={true} opacity={0.35}/>
+                        <meshStandardMaterial transparent={true} opacity={0.2}/>
                     </mesh>
                     <mesh position={[-0.5, 1.45, 0]}>
                         <boxGeometry args={[1, 0.1, 0.1]} />
@@ -49,15 +84,15 @@ export default function Experience()
                     </mesh>
                     <mesh position={[0, 0.85, 0]}>
                         <boxGeometry args={[0.25, 0.25, 0.25]} />
-                        <meshStandardMaterial transparent={true} opacity={0.5} color={0x0000ff} />
+                        <meshStandardMaterial transparent={true} opacity={0.25} color={0x0000ff} side={THREE.DoubleSide}/>
                     </mesh>
                     <mesh position={[0, 0.85, 0]}>
                         <boxGeometry args={[0.125, 0.125, 0.125]} />
-                        <meshStandardMaterial transparent={true} opacity={0.9} color={0x00ff00} />
+                        <meshPhongMaterial transparent={true} opacity={0.75} color={0x00ff00} side={THREE.DoubleSide}/>
                     </mesh>
-                </group>
+                </group> */}
                 
-            </PresentationControls>
+            {/* </PresentationControls> */}
             
             {/* Ground Mesh */}
             {/* <mesh rotation={[-Math.PI * 0.5, 0, 0]} receiveShadow>
@@ -65,14 +100,6 @@ export default function Experience()
                 <meshStandardMaterial side={THREE.DoubleSide}/>
             </mesh> */}
 
-        </Stage>
-        
-        
-        {/* Lights */}
-        <directionalLight position={[0, 2, 1]} color="red" intensity={1.}/>
-        <directionalLight position={[0, 2, -1]} color="red" intensity={1.}/>
-        <ambientLight intensity={0.125} />
-
-        
+        </Stage>  
     </>
 }
