d9cb5659b13be4ce1b134326bdfa5e6ea4c38da0
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-07-06 07:12:02 -0400

    update camera controls

--- src/Scrolly.jsx
+++ src/Scrolly.jsx
@@ -12,12 +12,13 @@ export default function Scrolly()
     const cameraPos3 = new THREE.Vector3(0, 0.5, -0.5)
 
     const specimenLocation = new THREE.Vector3(0, 0.0, 0)
+    const originalPosition = new THREE.Vector3(0, 2, 4)
 
     useFrame((state, delta) => {
         const r1 = scroll.range(0/4, 1/4)
-        console.log('r1: ' + r1)
+        // console.log('r1: ' + r1)
         const r2 = scroll.range(1/4, 2/4)
-        console.log('r2: ' + r2)
+        // console.log('r2: ' + r2)
         // console.log(state.camera.position)
         // state.camera.position.z -= r1 * 0.01
         // state.camera.lookAt(object.position)
@@ -25,12 +26,18 @@ export default function Scrolly()
             state.camera.position.lerp(cameraPos1, 0.01)
             state.camera.lookAt(specimenLocation)
         }
+        if(r1 <= 0.5){
+            state.camera.position.lerp(originalPosition, 0.01)
+            state.camera.lookAt(specimenLocation)
+        }
 
         if(r2 > 0.25 && r2 < 0.75){
             state.camera.position.lerp(cameraPos2, 0.01)
+            state.camera.lookAt(specimenLocation)
         }
         if(r2 > 0.75){
             state.camera.position.lerp(cameraPos3, 0.01)
+            state.camera.lookAt(specimenLocation)
         }
         
     })
